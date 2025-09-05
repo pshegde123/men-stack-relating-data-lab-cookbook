7573ce3a663a42bf6b0de8200baf1b476f10fa05
pshegde123 <pradnyahegde@gmail.com>
2025-09-05 17:40:54 -0400

    added user constroller

--- controllers/users.js
+++ controllers/users.js
@@ -7,11 +7,13 @@ router.get('/', async (req, res) => {
   res.render('users/index.ejs', { users: await User.find({}) });
 });
 router.get('/show/:userId', async (req, res) => {
-  console.log("show user pantry list....");
-  console.log(req.params.userId);
+  //console.log("show user pantry list....");
+  //console.log(req.params.userId);
+  const pantry_item_list = await User.findOne({ _id: req.params.userId }).populate('pantry');
+  //console.log(pantry_item_list);
   res.render('users/show.ejs', {
     user: await User.findById(req.params.userId),
-    pantry: await User.findById(req.params.userId).populate('pantry'),
+    pantry: [...pantry_item_list.pantry],
    });
 });
 module.exports = router;
